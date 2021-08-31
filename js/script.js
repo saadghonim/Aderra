3bb7927894aeda89726d318736230b398689282e
saadghonim <86053620+saadghonim@users.noreply.github.com>
2021-08-31 19:14:53 +0200

    edit css

---
+++ js/script.js
@@ -0,0 +1,149 @@
+$(document).ready(function() {
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~start header ~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */
+
+$(".trigger").click(function(){
+  $(".nav_bar").slideToggle(500);
+  $(".moboverlay").toggle('500');
+  $("body").toggleClass("over")
+  
+});
+
+$(".moboverlay").click(function(){
+  $(".moboverlay").fadeOut('500');
+  $(".nav_bar").slideToggle(500);
+  $(".bars").removeClass("active")
+  $("body").removeClass("over")
+});
+$(".btn_drop_lang").click(function(){
+  $(".ul_drop_lang").slideToggle(500)
+})
+
+$(".btn_drop_riyal").click(function(){
+  $(".ul_drop_riyal").slideToggle(500)
+})
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~end header  ~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */
+
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~start  slider~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */
+$('.slider .owl-carousel').owlCarousel({
+  loop: true,
+  nav: true,
+  rtl: true,
+  autoplay: true,
+  rtl: $("html").attr("dir") == "rtl" ? true : false,
+  navText: ["<span class='lnr lnr-chevron-right'></span>", "<span class='lnr lnr-chevron-left'></span>"],
+  responsive: {
+      0: {
+          items: 1
+      },
+      600: {
+          items: 1
+      },
+      1000: {
+          items: 1
+      }
+  }
+})
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~end slider ~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~start Products ~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */
+$('.products .owl-carousel').owlCarousel({
+  loop:true,
+  margin:30,
+  nav:true,
+  rtl: $("html").attr("dir") == "rtl" ? true : false,
+  navText: ["<span class='lnr lnr-chevron-right'></span>", "<span class='lnr lnr-chevron-left'></span>"],
+  responsive:{
+      0:{
+          items:2,
+          margin:15
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:4
+      }
+  }
+});
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~end  Products~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~start brands~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */
+$('.brands .owl-carousel').owlCarousel({
+loop: true,
+margin: 15,
+nav: true,
+rtl: $("html").attr("dir") == "rtl" ? true : false,
+navText: ["<span class='lnr lnr-chevron-right'></span>", "<span class='lnr lnr-chevron-left'></span>"],
+rtl: true,
+responsive: {
+    0: {
+        items: 2,
+        margin:15
+    },
+    600: {
+        items: 4
+    },
+    1000: {
+        items: 6
+    }
+}
+})
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~end brands~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  */
+// header_scroll..............
+
+});
+// scroll button
+var myButton = document.getElementById('goup');
+window.onscroll = function () {
+    "use strict";
+    if (window.pageYOffset >= 600) {
+        myButton.style.opacity = "1";
+
+    } else {
+        myButton.style.opacity = "0"
+    }
+};
+
+myButton.onclick = function () {
+    "use strict";
+    window.scroll({
+        top: 0,
+        left: 100,
+        behavior: 'smooth'
+    });
+
+
+};
+
+        /* ****************start wow ******************** */
+        new WOW().init();
+        document.documentElement.style.setProperty('--animate-duration', '.8s');
+        /* ****************end wow ******************** */
+
+        $(window).load(function() {
+          $(".preloader-sa").fadeOut();
+        });
